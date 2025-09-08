c8c7751b7ed221ff01bc2cd6299ea7ad66b21d42
Kareem ELKady <kareemelkady358@gmail.com>
2025-09-08 21:17:29 +0300

    update

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './product.service';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
